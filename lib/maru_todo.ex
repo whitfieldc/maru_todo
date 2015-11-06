9f6cfce01ec84b9d07a442bf63b054519db20387
Charles Whitfield <whitfield.cw@gmail.com>
2015-11-06 12:30:47 -0800

    add maru library and initial hello world

--- lib/maru_todo.ex
+++ lib/maru_todo.ex
@@ -1,2 +1,18 @@
-defmodule MaruTodo do
+defmodule MaruTodo.Router.Homepage do
+	use Maru.Router
+
+	get do
+    	%{ hello: :world }
+	end
+end
+
+defmodule MaruTodo.API do
+    use Maru.Router
+
+    mount MaruTodo.Router.Homepage
+
+	rescue_from :all do
+    	status 500
+    	"Server Error"
+    end
 end
