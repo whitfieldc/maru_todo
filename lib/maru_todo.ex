31fd7ee4beaa0c01963e400877104fbdc5bfbfbc
Charles Whitfield <whitfield.cw@gmail.com>
2015-11-08 21:42:11 -0600

    fix model and changeset funciton

--- lib/maru_todo.ex
+++ lib/maru_todo.ex
@@ -1,8 +1,28 @@
+defmodule MaruTodo.Repo do
+	use Ecto.Repo, otp_app: :maru
+end
+
+defmodule MaruTodo.Task do
+	use Ecto.Model
+	import Ecto.Changeset
+
+	schema "tasks" do
+		field :title
+	end
+
+	@required_fields ~w(title)
+
+	def changeset(task, params \\ :empty) do
+		task
+		|> cast(params, @required_fields)
+	end
+end
+
 defmodule MaruTodo.Router.Homepage do
 	use Maru.Router
 
-	import Ecto.Query
-	alias MaruTodo.Tasks
+	alias MaruTodo.Task
+	alias MaruTodo.Repo
 	# plug CORS
 	get do
     	%{ hello: :world }
@@ -11,30 +31,25 @@ defmodule MaruTodo.Router.Homepage do
 	post do
     	# IO.inspect(fetch_req_body)
     	body = fetch_req_body
-
-    	IO.inspect(body.body_params)
+    	changeset = Task.changeset(%Task{}, body.body_params)
+    	case Repo.insert(changeset) do
+    		{:ok, task} ->
+    			IO.inspect(task)
+    		{:error, changeset} ->
+    			status(400)
+    	end
+    	# IO.inspect(body.body_params)
 	end
 
-	patch do
-		
-	end
+	# patch do
 
-	delete do
+	# end
 
-	end
+	# delete do
+
+	# end
 end
 
-defmodule MaruTodo.Repo do
-	use Ecto.Repo, otp_app: :maru
-end
-
-defmodule MaruTodo.Tasks do
-	use Ecto.Model
-
-	schema "tasks" do
-		field :title
-	end
-end
 
 
 defmodule MaruTodo.API do
