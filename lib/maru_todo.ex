d00dcdcd2e1c164dc4e081f5dd69c50320630f62
Charles <whitfield.cw@gmail.com>
2015-11-16 21:22:41 -0800

    move api endpoint to api.ex

--- lib/maru_todo.ex
+++ lib/maru_todo.ex
@@ -140,16 +140,16 @@ end
 
 
 ########## maru_todo/api.ex
-defmodule MaruTodo.API do
-    use Maru.Router
-    plug CORSPlug
-    plug Plug.Logger
+# defmodule MaruTodo.API do
+#     use Maru.Router
+#     plug CORSPlug
+#     plug Plug.Logger
 
-    mount MaruTodo.Router.Homepage
+#     mount MaruTodo.Router.Homepage
 
-	rescue_from :all, as: e do
-		IO.inspect (e)
-    	status 500
-    	"Server Error"
-    end
-end
+# 	rescue_from :all, as: e do
+# 		IO.inspect (e)
+#     	status 500
+#     	"Server Error"
+#     end
+# end
